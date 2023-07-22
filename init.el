7cbceb1390ac4136b1429741cb13df4350e7462d
SophieBosio <sophie.bosio@outlook.com>
2023-07-23 00:57:30 +0200

    Removed eat and quelpa -- too long startup

--- init.el
+++ init.el
@@ -317,6 +317,14 @@ tangled, and the tangled file is compiled."
   ;; Center the terminal window by default with Olivetti
   (add-hook 'vterm-mode-hook 'olivetti-mode))
 
+(quelpa '(eat :fetcher git
+              :url "https://codeberg.org/akib/emacs-eat"
+              :files ("*.el" ("term" "term/*.el") "*.texi"
+                      "*.ti" ("terminfo/e" "terminfo/e/*")
+                      ("terminfo/65" "terminfo/65/*")
+                      ("integration" "integration/*")
+                      (:exclude ".dir-locals.el" "*-tests.el"))))
+
 (use-package dashboard
   :config
   (setq dashboard-display-icons-p     t) ;; display icons on both GUI and terminal
