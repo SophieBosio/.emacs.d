5185994461b5a4cd6375318b6409707b43234409
SophieBosio <sophie.bosio@outlook.com>
2023-07-25 22:56:17 +0200

    New corfu config

--- init.el
+++ init.el
@@ -341,7 +341,6 @@ tangled, and the tangled file is compiled."
   :config
   (vertico-mode 1)
   (setq vertico-count 25                       ; Show more candidates
-										; Hide unavailable commands
 		read-extended-command-predicate 'command-completion-default-include-p
 		read-file-name-completion-ignore-case t  ; Ignore case of file names
 		read-buffer-completion-ignore-case t     ; Ignore case in buffer completion
@@ -366,15 +365,38 @@ tangled, and the tangled file is compiled."
   (marginalia-mode 1))
 
 (use-package corfu
+  :custom
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
   :init
-  (global-corfu-mode 1)
-  (corfu-popupinfo-mode 1)
-  :config
-  (setq corfu-cycle t
-        corfu-auto t
-        corfu-auto-delay 0
-        corfu-auto-prefix 2
-        corfu-popupinfo-delay 0.5))
+  (global-corfu-mode))
+
+(use-package emacs
+  :init
+  ;; TAB cycle if there are only few candidates
+  (setq completion-cycle-threshold 3)
+
+  ;; Hide commands in M-x which do not apply to the current mode.
+  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
+  (setq read-extended-command-predicate
+        #'command-completion-default-include-p)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (setq tab-always-indent 'complete))
 
 (use-package orderless
   :ensure t
